922efbc0b94c0fc47f97e9f1b527b56d3544e0cf
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2013-04-07 17:17:06 +0100

    Turn config dirs into git repos

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-##########################################################
-##  Bootstrap script for system provisioning	##
-##	Alex Wilson <a.wilson@alumni.warwick.ac.uk>	##
-##########################################################
+#####################################################
+#    Bootstrap script for system provisioning       #
+#    Alex Wilson <a.wilson@alumni.warwick.ac.uk>    #
+#####################################################
 
 SALT_CONFIG_URL=https://api.github.com/repos/mrwilson/salt-config/tarball
 SALT_CONFIG_DIR=/srv/salt
 
 DOTFILES_URL=https://api.github.com/repos/mrwilson/dotfiles/tarball
 DOTFILES_DIR=/srv/salt-files
+
+GITHUB=git@github.com:mrwilson
 
 echo "Downloading minimal salt-minion"
 wget -O - http://bootstrap.saltstack.org | sh
@@ -31,3 +33,9 @@ salt-call state.highstate
 echo "Reassigning privileges"
 chown -R mrwilson:mrwilson $SALT_CONFIG_DIR
 chown -R mrwilson:mrwilson $DOTFILES_DIR
+
+echo "Transforming config directories into git repos."
+git clone --bare $GITHUB/dotfiles.git $DOTFILES_DIR/.git
+git clone --bare $GITHUB/salt-config.git $SALT_CONFIG_DIR/.git
+
+echo "Done."
